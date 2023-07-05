69f1226604775e92e4c6768bfb93626fc92cfee5
Your Name <you@example.com>
2023-07-05 15:39:23 +0200

    connection with database and wp-config working

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 
+sleep 30
 mkdir -p /var/www/html/wordpress
 cd /var/www/html/wordpress
 
@@ -7,16 +8,19 @@ rm -rf *
 
 wp --allow-root core download
 
-mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+# mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 
-sed -i -r "s/database_name_here/$DB_NAME/1" wp-config.php
-sed -i -r "s/username_here/$DB_USER/1" wp-config.php
-sed -i -r "s/password_here/$DB_PASSWORD/1" wp-config.php
-sed -i -r "s/localhost/$DB_HOST/1" wp-config.php
+# sed -i -r "s/database_name_here/$DB_NAME/1" wp-config.php
+# sed -i -r "s/username_here/$DB_USER/1" wp-config.php
+# sed -i -r "s/password_here/$DB_PASSWORD/1" wp-config.php
+# sed -i -r "s/localhost/$DB_HOST/1" wp-config.php
+
+wp --allow-root config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST"
 
 # installs WordPress and sets up the basic configuration for the site.
 # The --url option specifies the URL of the site, --title sets the site's title, --admin_user and --admin_password set the username and password 
 # for the site's administrator account, and --admin_email sets the email address for the administrator.
+
 wp --allow-root core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER--admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL
 
 # creates a new user account with the specified username, email address, and password.
