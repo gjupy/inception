bf78dc614e211e82ec1a90fcac6e2739a1470d44
Giuliano Jupy <94360193+gjupy@users.noreply.github.com>
2023-07-06 15:31:18 +0200

    Update create_user.sh

--- srcs/requirements/mariadb/conf/create_user.sh
+++ srcs/requirements/mariadb/conf/create_user.sh
@@ -6,13 +6,8 @@ service mysql start
 
 sleep 5
 
-if [ -d "/var/lib/mysql/$DB_NAME" ]
-then
-	echo "WARNING: Data base exist";
-else
-	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'; FLUSH PRIVILEGES;"
-	mysqladmin --no-defaults --user=root password "$DB_ROOT_PASSWORD"
-fi
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'; FLUSH PRIVILEGES;"
+mysqladmin --no-defaults --user=root password "$DB_ROOT_PASSWORD"
 
 service mysql stop
 
