cde633652399d3be5a08a01c9f02fba9a7f4a095
Giuliano Jupy <94360193+gjupy@users.noreply.github.com>
2023-07-07 15:18:48 +0200

    Update create_user.sh - mysql start and stop only needed to set up admin settings

--- srcs/requirements/mariadb/conf/create_user.sh
+++ srcs/requirements/mariadb/conf/create_user.sh
@@ -2,19 +2,16 @@
 
 mysql_install_db
 
-service mysql start
-
-sleep 5
-
 if [ -d "/var/lib/mysql/$DB_NAME" ]
 then
 	echo "WARNING: Database already exists. Skipping initialization steps.";
 else
+	service mysql start
+	sleep 5
 	echo "Initializing database..."
 	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'; FLUSH PRIVILEGES;"
-	# mysqladmin -u root password "$DB_ROOT_PASSWORD"
+	mysqladmin -u root password "$DB_ROOT_PASSWORD"
+	service mysql stop
 fi
-
-service mysql stop
 
 exec $@
